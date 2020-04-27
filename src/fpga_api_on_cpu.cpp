17afa87378f5a960fa0a44cdf2c6ff6db6953e62
givenone <givenone@gmail.com>
2020-04-27 06:15:18 +0000

    lab 7 done

--- src/fpga_api_on_cpu.cpp
+++ src/fpga_api_on_cpu.cpp
@@ -79,22 +79,37 @@ void FPGA::largeMV(const float *large_mat, const float *input, float *output, in
   {
     for (int j = 0; j < num_input; j += v_size_)
     {
-      // 0) Initialize input vector
-      int block_row = min(m_size_, num_output - i);
-      int block_col = min(v_size_, num_input - j);
+        // 0) Initialize input vector		
+        int block_row = min(m_size_, num_output-i);
+        int block_col = min(v_size_, num_input-j);
 
-      // 1) Assign a vector
-      // IMPLEMENT THIS
+        // !) Assign a vector
+        /* IMPLEMENT */
+        memcpy(vec, input + j, sizeof(float) * block_col);
+        //if(block_col < v_size_) memset()
+        // 2) Assign a matrix
+        /* IMPLEMENT */
+        int k=0;
+        for(; k< block_row ; k++)
+        {
+            memcpy(mat+ v_size_* k, large_mat + (i+k) * num_input + j, sizeof(float) * block_col);
+            if(block_col < v_size_) memset(mat+ v_size_ * k + block_col, 0, sizeof(float) * (v_size_ - block_col));
+        }
+        if(k < m_size_)
+        {
+            for(int x = 0; x < m_size_ - k ; x++)
+            {
+                memset(mat+ v_size_ * ( k + x ), 0, sizeof(float) * v_size_);
+            }
+        }
+        // 3) Call a function `block_call() to execute MV multiplication
+        const float* ret = this->blockMV();
 
-      // 2) Assign a matrix
-      // IMPLEMENT THIS
-
-      // 3) Call a function `blockMV() to execute MV multiplication
-      const float *ret = this->blockMV();
-
-      // 4) Accumulate intermediate results
-      for (int row = 0; row < block_row; ++row)
-        output[i + row] += ret[row];
+        // 4) Accumulate intermediate results
+        for(int row = 0; row < block_row; ++row)
+        {
+            output[i + row] += ret[row];
+        }
     }
   }
 }
@@ -121,9 +136,50 @@ void FPGA::convLowering(const std::vector<std::vector<std::vector<std::vector<fl
   //int input_channel = inputs.size();
   int input_height = inputs[0].size();
   int input_width = inputs[0][0].size();
-
   // IMPLEMENT THIS
   // For example,
   // new_weights[0][0] = cnn_weights[0][0][0][0];
   // new_inputs[0][0] = inputs[0][0][0];
+    //printf("%d %d %d %d\n", conv_channel, input_channel, conv_height, conv_width);
+    //printf("%d \n", cnn_weights.size());
+    for(int c=0; c<conv_channel; c++)
+    {
+      // vector row new_weight[c] 
+      int cnt = 0;
+      for(int ic = 0; ic<input_channel; ic++)
+      {
+          for(int h=0; h<conv_height; h++)
+          {
+              for(int w=0; w<conv_width; w++)
+              {
+                  new_weights[c][cnt++] = (cnn_weights[c][ic][h][w]);
+              }
+          }
+      }
+  }
+
+  // first move row-wise  
+  //printf("%d %d\n", input_height, input_width);
+  //printf("%d %d\n",new_inputs.size(), new_inputs[0].size());
+  int cnt = 0;
+  for(int y=0; y<input_height-conv_height+1; y++)
+  {
+      for(int x=0; x<input_width-conv_width+1; x++)
+      {
+          // input row new_input[cnt];
+          for(int ic=0; ic<input_channel; ic++)
+          {
+                for(int h=0; h<conv_height; h++)
+                {
+                    for(int w=0; w<conv_width; w++)
+                    {
+                        int idx = w + conv_width * h + (conv_width*conv_height) * ic;
+                        new_inputs[idx][cnt] = inputs[ic][h+y][w+x];
+                    }
+                }
+          }
+        cnt++;
+      }
+
+  }
 }
